b177d5943b06fee8ff569612b024beac8f44d8f5
michael <meo482@aucklanduni.ac.nz>
2020-04-01 18:24:00 +1300

    'fgf'

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
-import os
+import os, glob
+import json
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
+import pprint
 
-UPLOAD_FOLDER = '/Users/michael/Desktop/et/flsk/files'
+UPLOAD_FOLDER = r'C:\Users\mkeo2\Desktop\MapClient\wrcs'
 ALLOWED_EXTENSIONS = {'trc'}
 
 app = Flask(__name__)
@@ -14,10 +16,12 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -36,3 +40,33 @@ def upload_file():
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         return redirect(url_for('uploaded_file',
                                 filename=filename))
+
+
+@app.route('/get', methods=['GET'])
+def returnMarkerSet():
+    path = os.getcwd()
+    path = path + "\\"
+    trcFiles = []
+    for file in glob.glob("*.trc"):
+        trcFiles.append(file)
+    for trc in trcFiles:
+        with open(trc) as f:
+            lis = [line.split() for line in f]        # create a list of lists
+    metaData = lis[3]
+    metaData = metaData[2:]
+    dicts = {}
+    i = 1
+    for meta in metaData:
+        dicts[str(i)] = meta
+        i+=1
+    return ulify(metaData)
+
+
+def ulify(elements):
+    string = "<ul>\n"
+    for s in elements:
+        string += "<li>" + str(s) + "</li>\n"
+    string += "</ul>"
+    return string
+
+
